4e9d4302e5eeb1d1f276ecd6460affc89c39ce4d
abhiramkrishnan <abhiramkrishnan603@gmail.com>
2026-05-17 23:57:43 +0530

    Fixed CORS for frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,14 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://smart-leads-frontend-v12m.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(
