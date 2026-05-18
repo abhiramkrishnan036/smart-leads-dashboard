36247c1a69a83bf68af931815aa3d4f0942511f9
abhiramkrishnan <abhiramkrishnan603@gmail.com>
2026-05-18 11:18:27 +0530

    Fixed axios syntax

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    https://smart-leads-dashboard-lmq7.onrender.com/api,
+  baseURL: "https://smart-leads-dashboard-lmq7.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
